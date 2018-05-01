6b8be68490587f533be096a4ce03446165f0a0e1
Pedro Pais <Pedro Pais@DESKTOP-DFHBJ2Q.lan>
2018-05-01 10:51:35 +0100

    sfsdf

--- ADAtp/src/Graph.java
+++ ADAtp/src/Graph.java
@@ -5,8 +5,21 @@ import java.util.List;
 public class Graph {
 
 	List<LinkedList<Integer>> graph;
+	int[] inDegree, outDegree;
 	
 	public Graph(int numNodes){
 		graph = new ArrayList<LinkedList<Integer>>(numNodes);
+		inDegree = new int[numNodes];
+		outDegree = new int[numNodes];
 	}
+	
+	public int inDegree(int node) {
+		return inDegree[node];
+	}
+	
+	public int outDegree(int node) {
+		return outDegree[node];
+	}
+	
+	public Iterator
 }
