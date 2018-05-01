2105b3831a59303838070d348d3f1c5152ebd061
Pedro Pais <Pedro Pais@DESKTOP-DFHBJ2Q.lan>
2018-05-01 10:04:36 +0100

    Esqueleto done?

--- ADAtp/src/Problem.java
+++ ADAtp/src/Problem.java
@@ -1,3 +1,21 @@
-public class Problem {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Problem {
+	
+	int nSuspects;
+	List<Pair> Preceding, Concurrent;
+	
+	public Problem(int nSuspects, int nPreceding, int nConcurrent) {
+		Preceding = new ArrayList<Pair>(nPreceding);
+		Concurrent = new ArrayList<Pair>(nConcurrent);
+	}
+	
+	public void addPreceding(int x, int y) {
+		Preceding.add(new Pair(x,y));
+	}
+	
+	public void addConcurrent(int x, int y) {
+		Concurrent.add(new Pair(x,y));
+	}
 }
