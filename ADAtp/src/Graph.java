0697bb45beb1ecd7cfdb508e7400ea68476f7ca6
Pedro Pais <Pedro Pais@DESKTOP-DFHBJ2Q.lan>
2018-05-01 10:33:57 +0100

    fdg

---
+++ ADAtp/src/Graph.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+
+	List<Integer> graph;
+	
+	public Graph(int numNodes){
+		graph = new ArrayList<Integer>(numNodes);
+	}
+}
--- ADAtp/src/Problem.java
+++ ADAtp/src/Problem.java
@@ -25,4 +25,6 @@ public class Problem {
 		
 		return false;
 	}
+	
+	
 }
