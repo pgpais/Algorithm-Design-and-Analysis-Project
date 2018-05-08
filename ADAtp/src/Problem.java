d8df86b67a184c0a1b0ffdcf71e5174ca74739bf
Pedro Pais <Pedro Pais@DESKTOP-DFHBJ2Q.lan>
2018-05-08 18:03:35 +0100

    sfsd

--- ADAtp/src/Problem.java
+++ ADAtp/src/Problem.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Problem {
 	
@@ -25,12 +26,50 @@ public class Problem {
 		
 		Graph graph = new Graph(nSuspects, preceding);
 		
+		boolean[] processed = new boolean[nSuspects];
 		
+		/*
+		 * Checks every Pair with the graph.
+		 */
+		for(Pair p : concurrent) {
+			if(!dfsExplore(graph, p.getX(), p.getY(), processed)) {
+				if(!dfsExplore(graph, p.getY(), p.getX(), processed)) {
+					return false;
+				}
+			}
+		}
 		
-		return false;
+		return true;
 	}
 	
-	private 
+	
+	/*
+	 * Depth First Traversal - Tries to find the goal node, returns as soon as it is found.
+	 */
+	private boolean dfsExplore(Graph graph, int startNode, int goal, boolean[] processed) {
+		
+		Stack<Integer> foundUnprocessed = new Stack<Integer>();
+		foundUnprocessed.push(startNode);
+		
+		do {
+			int node = foundUnprocessed.pop();
+			if(!processed[node]) {
+				//Process
+				if(node == goal) {
+					return false;
+				}
+				processed[node] = true;
+				
+				for(int v : graph.outAdjacentNodes(node)) {
+					if(!processed[v]) {
+						foundUnprocessed.push(v);
+					}
+				}
+			}
+		} while(!foundUnprocessed.isEmpty());
+		
+		return true;
+	}
 	
 	
 }
