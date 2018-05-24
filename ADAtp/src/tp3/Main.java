be08597678d0d930763201648747983f65e7d2a9
Pedro Pais <ppais95@gmail.com>
2018-05-24 18:39:08 +0100

    --

--- ADAtp/src/tp3/Main.java
+++ ADAtp/src/tp3/Main.java
@@ -13,16 +13,25 @@ public class Main {
 		
 		String size = input.readLine();
 		//Send size.charAt(0) and size.charAt(2) to class Problem
-		Problem prob = new Problem(size.charAt(0), size.charAt(2));
-				
-		for(int i = 0; i < size.charAt(2); i++) {
+		
+		String[] sizes = size.split(" ");
+		
+		int w,h;
+		w = Integer.parseInt(sizes[0]);
+		h = Integer.parseInt(sizes[1]);
+		
+		Problem prob = new Problem(w, h);
+		
+		for(int i = 0; i < h; i++) {
 			String line = input.readLine();
-			char[] row = new char[size.charAt(0)];
-			for(int j = 0; j < size.charAt(0); j++) {
+			char[] row = new char[w];
+			for(int j = 0; j < row.length; j++) {
 				row[j] = line.charAt(j*2);
 			}
 			prob.addLine(row, i);
 		}
+		
+		System.out.println(prob.solve());
 	}
 
 }
--- ADAtp/src/tp3/Problem.java
+++ ADAtp/src/tp3/Problem.java
@@ -1,5 +1,6 @@
 package tp3;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -11,19 +12,19 @@ public class Problem {
 	int[][] lantern;
 	int[][] steps;
 
-	public Problem(char w, char h) {
+	public Problem(int w, int  h) {
 		map = new char[h][w];
 		lantern = new int[h][w];
-		
+
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
 				lantern[i][j] = -1;
 			}
 		}
-		
+
 		this.w = w;
 		this.h = h;
-		
+
 		lanternLevel = 0;
 	}
 
@@ -39,52 +40,65 @@ public class Problem {
 		return -1;
 	}
 
-	boolean canMove(int x1, int y1, int x2, int y2) {
-		if ((x2 >= 0 && x2 < w) || (y2 >= 0 && y2 < h)) {
-			if ((map[x2][y2] == '*' || map[x1][y1] == '*')) {
+	boolean canMove(int[] node, int x2, int y2) {
+		if (x2 >= 0 && x2 < w && y2 >= 0 && y2 < h) {
+			if ((map[x2][y2] == '*' || map[node[0]][node[1]] == '*')) {
 				return true;
-			} else if (lanternLevel > 0)
+			} else if (node[2] > 0)
 				return true;
 		}
 		return false;
 	}
 
-	void bfsExplore() {
-		Queue<int[]> waiting = new PriorityQueue<int[]>();
-		waiting.add(createNode(0, 0));
+	public int solve() {
+		Queue<int[]> waiting = new LinkedList<int[]>();
+		waiting.add(new int[] { 0, 0, 0, 0 });
 
 		do {
-			//[0] x [1] y [2] lanterLevel [3] dist
+			// [0] x, [1] y, [2] lanternLevel, [3] dist.
 			int[] node = waiting.poll();
 			// process
-			if(node[0] == w-- && node[1] == h--) {
-				//done!
+			if (node[0] == w-1 && node[1] == h-1) {
+				return node[3];
 			}
-			if (canMove(node[0], node[1], node[0]++, node[1])) {
-				waiting.add(moveTo(node[0], node[1], node[0]++, node[1]));
+			if (canMove(node, node[0]+1, node[1])) {
+				waiting.add(moveTo(node, node[0]+1, node[1]));
 			}
-			if (canMove(node[0], node[1], node[0]--, node[1])) {
-				waiting.add(moveTo(node[0], node[1], node[0]--, node[1]));
+			if (canMove(node, node[0]-1, node[1])) {
+				waiting.add(moveTo(node, node[0]-1, node[1]));
 			}
-			if (canMove(node[0], node[1], node[0], node[1]++)) {
-				waiting.add(moveTo(node[0], node[1], node[0], node[1]++));
+			if (canMove(node, node[0], node[1]+1)) {
+				waiting.add(moveTo(node, node[0], node[1]+1));
 			}
-			if (canMove(node[0], node[1], node[0], node[1]--)) {
-				waiting.add(moveTo(node[0], node[1], node[0], node[1]--));
+			if (canMove(node, node[0], node[1]-1)) {
+				waiting.add(moveTo(node, node[0], node[1]-1));
 			}
 
 		} while (!waiting.isEmpty());
-
+		return -1;
 	}
 
-	int[] moveTo(int i, int j, int k, int l) {
-		
-		
-		
-		return new int[] {-1,-1};
+	int[] moveTo(int[] node, int x2, int y2) {
+		int lantern = node[2];
+
+		// se nao for lampada, reduz lantern
+		if (map[x2][y2] != '*') {
+			// se for lantern, substitui se tiver mais carga
+			int mapLight = Character.getNumericValue(map[x2][y2]);
+			if (mapLight > lantern) {
+				lantern = mapLight;
+			}
+			if (map[node[0]][node[1]] != '*') {
+				lantern--;
+				if (lantern < 0)
+					System.err.println("negative lantern");
+			}
+		}
+
+		return new int[] { x2, y2, lantern, node[3]++ };
 	}
 
-	int[] createNode(int x, int y) {
+	int[] createNode(int x, int y, int lanternLevel, int dist) {
 		return new int[] { x, y };
 	}
 
